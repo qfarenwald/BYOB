911b01545aba1a57fd62c1b4d84db38ce6c7cd08
Quinne Farenwald <q.farenwald@gmail.com>
2019-11-20 19:10:34 -0700

    Set up initial migration

---
+++ db/migrations/20191120181420_initial.js
@@ -0,0 +1,32 @@
+
+exports.up = function(knex) {
+  return Promise.all([
+    knex.schema.createTable('themes', function(table) {
+      table.increments('inc_id').primary();
+      table.integer('id');
+      table.unique('id');
+      table.string('name');
+      table.integer('parent_id');
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable('sets', function(table) {
+      table.increments('inc_id').primary();
+      table.integer('set_num');
+      table.string('name');
+      table.integer('year');
+      table.integer('theme_id').unsigned();
+      table.foreign('theme_id').references('themes.id');
+      table.integer('num_parts');
+      table.timestamps(true, true);
+    })
+  ])
+};
+
+exports.down = function(knex) {
+  return Promise.all([
+    knex.schema.dropTable('sets'),
+    knex.schema.dropTable('themes')
+  ])
+
+};
