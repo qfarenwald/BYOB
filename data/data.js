ff266fe3e1675333c23ba16cfdafc956aa60e05f
Quinne Farenwald <q.farenwald@gmail.com>
2019-11-21 10:24:12 -0700

    Write cleanSets func to remove hyphen from set_num

--- data/data.js
+++ data/data.js
@@ -10,6 +10,7 @@ csv()
     console.log(jsonObj)
     return JSON.stringify(jsonObj)
   })
+
   .then((jsonArr) => {
     fs.writeFile('setsData.json', jsonArr, (err) => {
       if (err) throw err;
@@ -30,3 +31,19 @@ csv()
       console.log('Data is now written to the file')
     })
   })
+
+
+let sets = [
+  { set_num: '00-1', name: 'Weetabix Castle', year: '1970', theme_id: '414', num_parts: '471' },
+  { set_num: '0011-2', name: 'Town Mini-Figures', year: '1978', theme_id: '84', num_parts: '12' },
+  { set_num: '0011-3', name: 'Castle 2 for 1 Bonus Offer', year: '1987', theme_id: '199', num_parts: '2' },
+  { set_num: '0012-1', name: 'Space Mini-Figures', year: '1979', theme_id: '143', num_parts: '12' },
+  { set_num: '0013-1', name: 'Space Mini-Figures', year: '1979', theme_id: '143', num_parts: '12' }
+]
+
+const cleanSets = () => {
+  return sets.map((set) => {
+    set.set_num = set.set_num.replace(/-/gi, '');
+    return set
+  })
+}
