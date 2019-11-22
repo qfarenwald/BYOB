c5c529c20284512d3552df560b5c5ad7849d3068
Quinne Farenwald <q.farenwald@gmail.com>
2019-11-22 10:25:23 -0700

    Add seeds to production in knexfile to seed data on heroku

--- knexfile.js
+++ knexfile.js
@@ -11,11 +11,14 @@ module.exports = {
     useNullAsDefault: true
   },
   production: {
-  client: 'pg',
-  connection: process.env.DATABASE_URL + `?ssl=true`,
-  migrations: {
-    directory: './db/migrations'
-  },
-  useNullAsDefault: true
-}
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  }
 };
